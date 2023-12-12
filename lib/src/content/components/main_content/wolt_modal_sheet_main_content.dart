d1c3e6ec3eafe0d65db106dfb91e5736eab71303
Cagatay Ulusoy <cagatay.ulusoy@wolt.com>
2023-12-12 15:06:55 +0300

    fix format

--- lib/src/content/components/main_content/wolt_modal_sheet_main_content.dart
+++ lib/src/content/components/main_content/wolt_modal_sheet_main_content.dart
@@ -45,17 +45,22 @@ class _WoltModalSheetMainContentState extends State<WoltModalSheetMainContent> {
     final page = widget.page;
     final heroImageHeight = page.heroImage == null
         ? 0.0
-        : (page.heroImageHeight ?? themeData?.heroImageHeight ?? defaultThemeData.heroImageHeight);
-    final pageHasTopBarLayer =
-        page.hasTopBarLayer ?? themeData?.hasTopBarLayer ?? defaultThemeData.hasTopBarLayer;
+        : (page.heroImageHeight ??
+            themeData?.heroImageHeight ??
+            defaultThemeData.heroImageHeight);
+    final pageHasTopBarLayer = page.hasTopBarLayer ??
+        themeData?.hasTopBarLayer ??
+        defaultThemeData.hasTopBarLayer;
     final isTopBarLayerAlwaysVisible =
         pageHasTopBarLayer && page.isTopBarLayerAlwaysVisible == true;
-    final navBarHeight =
-        page.navBarHeight ?? themeData?.navBarHeight ?? defaultThemeData.navBarHeight;
-    final topBarHeight =
-        pageHasTopBarLayer || page.leadingNavBarWidget != null || page.trailingNavBarWidget != null
-            ? navBarHeight
-            : 0.0;
+    final navBarHeight = page.navBarHeight ??
+        themeData?.navBarHeight ??
+        defaultThemeData.navBarHeight;
+    final topBarHeight = pageHasTopBarLayer ||
+            page.leadingNavBarWidget != null ||
+            page.trailingNavBarWidget != null
+        ? navBarHeight
+        : 0.0;
     final scrollView = CustomScrollView(
       scrollBehavior: const DragScrollBehavior(),
       shrinkWrap: true,
@@ -76,7 +81,8 @@ class _WoltModalSheetMainContentState extends State<WoltModalSheetMainContent> {
                     // If top bar layer is always visible, the padding is explicitly added to the
                     // scroll view since top bar will not be integrated to scroll view at all.
                     // Otherwise, we implicitly create a spacing as a part of the scroll view.
-                    : SizedBox(height: isTopBarLayerAlwaysVisible ? 0 : topBarHeight);
+                    : SizedBox(
+                        height: isTopBarLayerAlwaysVisible ? 0 : topBarHeight);
               } else {
                 final pageTitle = page.pageTitle;
                 return KeyedSubtree(
@@ -98,17 +104,20 @@ class _WoltModalSheetMainContentState extends State<WoltModalSheetMainContent> {
     );
     return NotificationListener<ScrollNotification>(
       onNotification: (scrollNotification) {
-        final isVerticalScrollNotification = scrollNotification is ScrollUpdateNotification &&
-            scrollNotification.metrics.axis == Axis.vertical;
+        final isVerticalScrollNotification =
+            scrollNotification is ScrollUpdateNotification &&
+                scrollNotification.metrics.axis == Axis.vertical;
         if (isVerticalScrollNotification) {
-          widget.currentScrollPosition.value = scrollNotification.metrics.pixels;
+          widget.currentScrollPosition.value =
+              scrollNotification.metrics.pixels;
         }
         return false;
       },
       child: Padding(
         // The scroll view should be padded by the height of the top bar layer if it's always
         // visible. Otherwise, over scroll effect will not be visible due to the top bar layer.
-        padding: EdgeInsets.only(top: isTopBarLayerAlwaysVisible ? topBarHeight : 0),
+        padding:
+            EdgeInsets.only(top: isTopBarLayerAlwaysVisible ? topBarHeight : 0),
         child: scrollView,
       ),
     );
