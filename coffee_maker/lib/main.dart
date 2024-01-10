92ebe79afdd1ba5db556d44c0878c3b3ce219350
Yunus Emre Bakir <yunus.emre.bakir@wolt.com>
2024-01-10 16:04:53 +0100

    lint fix

--- coffee_maker/lib/main.dart
+++ coffee_maker/lib/main.dart
@@ -86,15 +86,18 @@ class _DemoAppState extends State<DemoApp> {
           ),
           outlinedButtonTheme: OutlinedButtonThemeData(
             style: OutlinedButton.styleFrom(
-              textStyle:
-                  Theme.of(context).textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.bold),
+              textStyle: Theme.of(context)
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(fontWeight: FontWeight.bold),
               foregroundColor: DemoAppColors.black,
               backgroundColor: DemoAppColors.white,
               surfaceTintColor: DemoAppColors.white,
               fixedSize: const Size.fromHeight(36),
               padding: const EdgeInsets.all(12),
               visualDensity: VisualDensity.compact,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8)),
               side: BorderSide.none,
             ),
           ),
@@ -117,7 +120,8 @@ class _DemoAppState extends State<DemoApp> {
           ),
         ),
         child: HomeScreen(
-          groupedCoffeeOrders: GroupedCoffeeOrders.fromCoffeeOrders(mockCoffeeOrders),
+          groupedCoffeeOrders:
+              GroupedCoffeeOrders.fromCoffeeOrders(mockCoffeeOrders),
           isStoreOnline: true,
           isGridOverlayVisible: false,
         ),
