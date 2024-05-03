b82abc398b7727f924a570433f48da99e08c81b7
furkan acar <65075121+AcarFurkan@users.noreply.github.com>
2024-05-03 20:03:32 +0300

    Update main.dart

--- playground/lib/main.dart
+++ playground/lib/main.dart
@@ -68,9 +68,8 @@ class _DemoAppState extends State<DemoApp> {
         brightness: Brightness.light,
         inputDecorationTheme: inputDecorationTheme,
         primaryColor: WoltColors.blue,
-        useMaterial3: true,
         switchTheme: SwitchThemeData(
-          thumbColor: MaterialStateProperty.all(WoltColors.blue),
+          thumbColor: MaterialStateProperty.all(WoltColors.black),
           trackColor: MaterialStateProperty.all(WoltColors.blue16),
         ),
         extensions: const <ThemeExtension>[
@@ -83,7 +82,6 @@ class _DemoAppState extends State<DemoApp> {
         brightness: Brightness.dark,
         inputDecorationTheme: inputDecorationTheme,
         primaryColor: WoltColors.blue,
-        useMaterial3: true,
         switchTheme: SwitchThemeData(
           thumbColor: MaterialStateProperty.all(WoltColors.blue),
           trackColor: MaterialStateProperty.all(WoltColors.blue16),
