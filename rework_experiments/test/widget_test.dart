2af4c8784a9636d8889dfeade877d2aaa3880a92
KristinaZoteva <kristina1994misha@gmail.com>
2024-10-17 13:48:35 +0200

    Chore: remove analyzers errors.

--- rework_experiments/test/widget_test.dart
+++ rework_experiments/test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:rework_experiments/navigation/example.dart';
+import 'package:rework_experiments/navigation_experiment/example.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
